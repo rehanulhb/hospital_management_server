d9b24eb9540c160622a26306b974076e0db8c151
rehanulhb <rehanulh@gmail.com>
2026-01-18 02:19:27 +0100

    Wrote Prisma Schema for User, Doctor and Patient

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import cors from "cors";
+
+import notFound from "./app/middlewares/notFound.js";
+import config from "./config/index.js";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler.js";
+
+const app: Application = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Server is Running",
+    environment: config.node_env,
+    uptime: process.uptime().toFixed(2) + " Sec",
+    timeStamp: new Date().toISOString(),
+  });
+});
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
---
+++ src/app/middlewares/notFound.ts
@@ -0,0 +1,12 @@
+import type { NextFunction, Request, Response } from "express";
+import status from "http-status";
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  return res.status(status.NOT_FOUND).json({
+    success: false,
+    message: "API Not Found !!",
+    error: "",
+  });
+};
+
+export default notFound;
