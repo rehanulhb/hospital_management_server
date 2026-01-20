e9dba5ffe435f6077257472ca6be7527aa1f333e
rehanulhb <rehanulh@gmail.com>
2026-01-20 02:42:10 +0100

    Uploaded Image to Cloudinary -Done

--- src/app/helper/fileUploader.ts
+++ src/app/helper/fileUploader.ts
@@ -1,6 +1,7 @@
 import multer from "multer";
 import path from "path";
 import { v2 as cloudinary } from "cloudinary";
+import config from "../../config/index.js";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -14,7 +15,25 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const uploadToCloudinary = async (file: Express.Multer.File) => {};
+const uploadToCloudinary = async (file: Express.Multer.File) => {
+  // Configuration
+  cloudinary.config({
+    cloud_name: config.cloudinary.cloud_name as string,
+    api_key: config.cloudinary.api_key as string,
+    api_secret: config.cloudinary.api_secret as string,
+  });
+
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(file.path, {
+      public_id: file.filename,
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return uploadResult;
+};
 
 export const fileUploaded = {
   upload,
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,5 +1,4 @@
 import bcrypt from "bcryptjs";
-import type { createPatientInput } from "./user.interface.js";
 import { prisma } from "../../shared/prisma.js";
 import type { Request } from "express";
 import { fileUploaded } from "../../helper/fileUploader.js";
@@ -7,27 +6,23 @@ import { fileUploaded } from "../../helper/fileUploader.js";
 const createPatient = async (req: Request) => {
   if (req.file) {
     const uploadResult = await fileUploaded.uploadToCloudinary(req.file);
-    console.log(uploadResult);
+    req.body.patient.profilePhoto = uploadResult?.secure_url;
   }
-  // const hashPassword = await bcrypt.hash(req.body.password, 10);
+  const hashPassword = await bcrypt.hash(req.body.password, 10);
 
-  // //   const result = await prisma.$transaction(async (tnx) => {});
-  // const result = await prisma.$transaction(async (tnx) => {
-  //   await tnx.user.create({
-  //     data: {
-  //       email: req.body.email,
-  //       password: hashPassword,
-  //     },
-  //   });
+  const result = await prisma.$transaction(async (tnx) => {
+    await tnx.user.create({
+      data: {
+        email: req.body.patient.email,
+        password: hashPassword,
+      },
+    });
 
-  //   return await tnx.patient.create({
-  //     data: {
-  //       name: req.body.name,
-  //       email: req.body.email,
-  //     },
-  //   });
-  // });
-  // return result;
+    return await tnx.patient.create({
+      data: req.body.patient,
+    });
+  });
+  return result;
 };
 
 export const UserService = {
