bc3b9296d7d927777767764f7e29e70609002bc8
rehanulhb <rehanulh@gmail.com>
2026-01-28 00:16:53 +0100

    Prisma Error Handling - added few error Defination

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -9,7 +9,7 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  let statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+  let statusCode: number = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
   let success = false;
   let message = err.message || "Something went wrong!";
   let error = err;
@@ -18,15 +18,30 @@ const globalErrorHandler = (
     if (err.code === "P2002") {
       message = "Duplicate Key error";
       error = err.meta;
+      statusCode = httpStatus.CONFLICT;
     }
     if (err.code === "P1000") {
       message = "Authentication failed against database server";
       error = err.meta;
+      statusCode = httpStatus.BAD_GATEWAY;
     }
     if (err.code === "P2003") {
       message = "Forgeign key contraint failed";
       error = err.meta;
+      statusCode = httpStatus.BAD_REQUEST;
     }
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    message = "Validation Error";
+    error = err.message;
+    statusCode = httpStatus.BAD_REQUEST;
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    message = "Unknown Prisma Error Occured";
+    error = err.message;
+    statusCode = httpStatus.BAD_REQUEST;
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    message = "Prisma Clinet failed to initialize Error Occured";
+    error = err.message;
+    statusCode = httpStatus.BAD_REQUEST;
   }
 
   return res.status(statusCode).json({
